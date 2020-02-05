189df7fece9c4ee50bbafc7ae05690865642a2be
Lucas Mazel <lucas.mazel@g-mail.fr>
2020-02-05 21:15:42 +0100

    adding repository

---
+++ src/repositories/recipe-food.repository.ts
@@ -0,0 +1,17 @@
+import {DefaultCrudRepository} from '@loopback/repository';
+import {
+  RecipeFood,
+  RecipeFoodRelations,
+} from '../models/recipe/recipe-food.model';
+import {MongoDsDataSource} from '../datasources';
+import {inject} from '@loopback/core';
+
+export class RecipeFoodRepository extends DefaultCrudRepository<
+  RecipeFood,
+  typeof RecipeFood.prototype.id,
+  RecipeFoodRelations
+> {
+  constructor(@inject('datasources.mongoDS') dataSource: MongoDsDataSource) {
+    super(RecipeFood, dataSource);
+  }
+}
