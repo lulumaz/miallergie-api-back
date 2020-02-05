64e1f335553c4942fcd8be73ef5b5c1408e633c4
Lucas Mazel <lucas.mazel@g-mail.fr>
2020-02-05 21:34:02 +0100

    adding relation recipe and diet

--- src/repositories/recipe.repository.ts
+++ src/repositories/recipe.repository.ts
@@ -1,16 +1,22 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {Recipe, RecipeRelations} from '../models';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {Recipe, RecipeRelations, Diet} from '../models';
 import {MongoDsDataSource} from '../datasources';
-import {inject} from '@loopback/core';
+import {inject, Getter} from '@loopback/core';
+import {DietRepository} from './diet.repository';
 
 export class RecipeRepository extends DefaultCrudRepository<
   Recipe,
   typeof Recipe.prototype.id,
   RecipeRelations
 > {
+
+  public readonly diet: BelongsToAccessor<Diet, typeof Recipe.prototype.id>;
+
   constructor(
-    @inject('datasources.mongoDS') dataSource: MongoDsDataSource,
+    @inject('datasources.mongoDS') dataSource: MongoDsDataSource, @repository.getter('DietRepository') protected dietRepositoryGetter: Getter<DietRepository>,
   ) {
     super(Recipe, dataSource);
+    this.diet = this.createBelongsToAccessorFor('diet', dietRepositoryGetter,);
+    this.registerInclusionResolver('diet', this.diet.inclusionResolver);
   }
 }
